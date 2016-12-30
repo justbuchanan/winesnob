ba1d278e08cc88660bdb2e2e2d884975e60a6ee4
Justin Buchanan <justbuchanan@gmail.com>
2016-12-29 22:02:50 -0800

    working on backend testing, updated todos

--- go/src/backend/backend_test.go
+++ go/src/backend/backend_test.go
@@ -7,6 +7,12 @@ import (
     "log"
     "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/sqlite"
+    // "github.com/gorilla/sessions"
+    "fmt"
+    "bytes"
+    "encoding/json"
+    "backend/apiai"
+    "io/ioutil"
 )
 
 func TestJoinWordSeries(t *testing.T) {
@@ -41,4 +47,60 @@ func TestApi(t *testing.T) {
     if res.StatusCode != http.StatusForbidden {
         t.Fatal("Api should be blocked when not authenticated")
     }
+
+    // var sess *Session
+    // sess, err = store.Getjk
+    req, _ := http.NewRequest("GET", ts.URL + "/api/wines", nil)
+    ForceAuthenticate(req, "justbuchanan@gmail.com")
+    fmt.Println("Force-authenticated as justbuchanan@gmail.com")
+
+
+    res, err = http.Get(ts.URL + "/api/wines")
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(store)
+    if res.StatusCode == http.StatusForbidden {
+        // t.Fatal("Api should be accessible after user is authenticated")
+        // TODO
+    }
+
+    actionResponse := GetActionResponse(t, ts, &apiai.ActionRequest{})
+    fmt.Println(actionResponse)
+}
+
+func GetActionResponse(t *testing.T, ts *httptest.Server, req *apiai.ActionRequest) *apiai.ActionResponse {
+    jsonValue, _ := json.Marshal(req)
+    res, err := http.Post(ts.URL + "/webhook", "application/json", bytes.NewBuffer(jsonValue))
+    if err != nil {
+        t.Fatal(err)
+        return nil
+    }
+
+    var apiResp apiai.ActionResponse
+    body, _ := ioutil.ReadAll(res.Body)
+    if err != nil {
+        t.Fatal(err)
+    }
+    err = json.Unmarshal(body, &apiResp)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    return &apiResp
+}
+
+
+func ForceAuthenticate(req *http.Request, email string) {
+    session, err := store.Get(req, "session-name")
+    if err != nil {
+        log.Fatal(err)
+        return
+    }
+
+    session.Values["email"] = email
+    w := httptest.NewRecorder()
+    session.Save(req, w)
+
+    fmt.Println(session)
 }
