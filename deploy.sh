bea9b34d7114eee525143c26482327f893d005fc
Justin Buchanan <justbuchanan@gmail.com>
2017-01-07 22:56:08 -0800

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function remote_run() {
+    ssh justbuchanan.com "cd cellar; $1"
+}
+
+echo "=> Prepare remote server for push"
+remote_run "git checkout master"
+
+branch="oauth2"
+
+echo "=> Push latest code to server"
+git push prod $branch
+
+echo "=> Rebuilding and re-running docker image"
+remote_run "git checkout $branch; docker-compose down && docker-compose up --build -d"
