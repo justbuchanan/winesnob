84173557174016d2cfc77c711875385eed7df794
Justin Buchanan <justbuchanan@gmail.com>
2017-01-11 20:41:33 -0800

    added SendError()

--- backend/api_handlers.go
+++ backend/api_handlers.go
@@ -9,13 +9,13 @@ import (
 	"net/http"
 )
 
-type ErrorResponse struct {
-	Error string `json:"error"`
+func SendError(w http.ResponseWriter, statusCode int, text string) {
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(map[string]string{"error": text})
 }
 
 func (env *Env) WineDeleteHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	wineID := vars["wineId"]
+	wineID := mux.Vars(r)["wineId"]
 
 	err := env.db.Delete(&WineInfo{ID: wineID}).Error
 	if err != nil {
@@ -30,8 +30,7 @@ func (env *Env) WineCreateHandler(w http.ResponseWriter, r *http.Request) {
 	var wine WineInfo
 	err := json.NewDecoder(r.Body).Decode(&wine)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(ErrorResponse{Error: "Invalid JSON"})
+		SendError(w, http.StatusBadRequest, "Invalid JSON")
 		return
 	}
 
@@ -41,8 +40,7 @@ func (env *Env) WineCreateHandler(w http.ResponseWriter, r *http.Request) {
 	// try to create a new wine in the db
 	err = env.db.Create(&wine).Error
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(ErrorResponse{Error: err.Error()})
+		SendError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
@@ -51,16 +49,14 @@ func (env *Env) WineCreateHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (env *Env) WineUpdateHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	wineID := vars["wineId"]
+	wineID := mux.Vars(r)["wineId"]
 
 	// TODO: ensure wineID already exists?
 
 	var wine WineInfo
 	err := json.NewDecoder(r.Body).Decode(&wine)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(ErrorResponse{Error: "Invalid JSON"})
+		SendError(w, http.StatusBadRequest, "Invalid JSON")
 		return
 	}
 
@@ -68,8 +64,7 @@ func (env *Env) WineUpdateHandler(w http.ResponseWriter, r *http.Request) {
 
 	err = env.db.Save(wine).Error
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(ErrorResponse{Error: "Invalid JSON"})
+		SendError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
@@ -77,16 +72,14 @@ func (env *Env) WineUpdateHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (env *Env) WineHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	wineID := vars["wineId"]
+	wineID := mux.Vars(r)["wineId"]
 
 	var wine WineInfo
 	err := env.db.Where(&WineInfo{ID: wineID}).First(&wine).Error
 
 	// 404 if no wine exists with that id
 	if err == gorm.ErrRecordNotFound {
-		w.WriteHeader(http.StatusNotFound)
-		json.NewEncoder(w).Encode(ErrorResponse{Error: "No wine found for id " + wineID})
+		SendError(w, http.StatusNotFound, "No wine found for id "+wineID)
 		return
 	}
 
